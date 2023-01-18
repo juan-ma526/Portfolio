564c3fa21f248c0ea6e2e17f6e67a4abd495ec01
juan-ma526 <jmperez675@gmail.com>
2023-01-18 11:46:30 -0300

    config

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,13 @@
+import Home from "./components/Home";
 import Navbar from "./components/NavBar";
+import Projects from "./components/Projects";
 
 function App() {
   return (
-    <div className="container-app">
+    <div className="font-Poppins bg-purple-800">
       <Navbar />
+      <Home />
+      <Projects />
     </div>
   );
 }
--- portfolio/src/data.js
+++ portfolio/src/data.js
@@ -1,3 +1,7 @@
+import project1 from "./assets/project1.jpg";
+import project2 from "./assets/project2.jpg";
+import project3 from "./assets/project3.jpg";
+
 export const navLinks = [
   {
     id: 1,
@@ -18,5 +22,30 @@ export const navLinks = [
     id: 4,
     text: "Contact",
     href: "contact",
+  },
+];
+
+export const btns = [
+  { id: 1, name: "All", value: "all" },
+  { id: 2, name: "Turno App", value: "Turno App" },
+  { id: 3, name: "Tmdb", value: "Tmdb" },
+  { id: 4, name: "Ecommerce", value: "Ecommerce" },
+];
+
+export const projects = [
+  {
+    id: 1,
+    image: project1,
+    value: "Turno App",
+  },
+  {
+    id: 2,
+    image: project2,
+    value: "Tmdb",
+  },
+  {
+    id: 3,
+    image: project3,
+    value: "Ecommerce",
   },
 ];
