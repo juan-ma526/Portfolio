d24eb0e883fabf34c21716cffe05659cb20f8cf5
juan-ma526 <jmperez675@gmail.com>
2023-01-18 11:46:39 -0300

    creacion navBar

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -7,7 +7,7 @@ import NavLinks from "./NavLinks";
 function Navbar() {
   const [toggle, setToggle] = useState(false);
   return (
-    <div className="fixed w-full top-0 left-0 z-20 bg-indigo-800">
+    <div className="fixed w-full top-0 left-0 z-20 bg-purple-800">
       <div className="container mx-auto flex items-center justify-between py-4 px-2 ">
         <div className="flex items-center gap-2">
           <div className="rounded-full w-8 h-8 bg-white flex items-center justify-center text-black font-bold">
@@ -40,7 +40,7 @@ function Navbar() {
         <motion.div
           initial={{ x: 500 }}
           animate={{ x: 0 }}
-          className="fixed right-0 top-0 bg-indigo-800 h-screen text-white w-80"
+          className="fixed right-0 top-0 bg-purple-800 h-screen text-white w-80"
         >
           <div className="flex flex-col gap-8 mt-[40%] ml-[30%]">
             {navLinks.map((navLink) => {
